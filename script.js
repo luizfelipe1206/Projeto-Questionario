a52d95581ebcb8b98b3ffe62b55cdf21481c4f0b
Luiz Felipe <luizdantas490@yahoo.com>
2024-02-08 11:24:45 -0300

    att

--- script.js
+++ script.js
@@ -94,6 +94,10 @@ const perguntas = [
 const quiz = document.querySelector('#quiz')
 const template = document.querySelector('template')
 
+const corretas = new Set()
+const totalDePerguntas = perguntas.length
+const mostrarTotal =document.querySelector('#acertos span')
+
 for(const item of perguntas){
     const quizItem = template.content.cloneNode(true)
     quizItem.querySelector('h3').textContent = item.pergunta
@@ -103,7 +107,20 @@ for(const item of perguntas){
     for(let resposta of item.respostas){
         const dt = quizItem.querySelector('dl dt').cloneNode(true)
         dt.querySelector('span').textContent = resposta
-        
+        dt.querySelector('input').setAttribute('name' , 'pergunta-' + perguntas.indexOf(item))
+        dt.querySelector('input').value = item.respostas.indexOf(resposta)
+
+        dt.querySelector('input').onchange = (event) => {
+            const estaCorreta = event.target.value == item.correta
+
+            corretas.delete(item)
+            if(estaCorreta) {
+                corretas.add(item)
+            }
+            
+        }
+
+
 
 
         quizItem.querySelector('dl').appendChild(dt)
